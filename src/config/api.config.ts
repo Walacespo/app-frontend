04b76d618bb524324a26ba4df76d92e4e229e5a7
Walace da Silva <walacespo@gmail.com>
2022-05-14 14:43:32 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    baseUrl : "http://localhost:8080",
+    baseUrl : "http://localhost:8080/",
     bucketBaseUrl : "https://curso-spring-ionic22.s3.sa-east-1.amazonaws.com"
 }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -33,6 +33,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -73,5 +77,19 @@ export class ProfilePage {
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+    .subscribe(Response => {
+      this.picture = null;
+      this.loadData();
+    },
+     error => {
+     })
+  }
+
+  cancel(){
+    this.picture = null;
   }
 }
